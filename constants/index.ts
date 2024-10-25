2179d8aa1fb200d8629e944be9675dc65a429654
Oscar Hernandez <oscarhernandezsoler@gmail.com>
2024-10-25 11:32:50 +0200

    new portfolio

---
+++ constants/index.ts
@@ -0,0 +1,137 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Java",
+    Image: "/java.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Kotlin",
+    Image: "/kotlin.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Dart",
+    Image: "/dart.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Flutter",
+    Image: "/flutter.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React Native",
+    Image: "/react_native.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "MySQL",
+    Image: "/mysql.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "Firebase",
+    Image: "/firebase.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Android",
+    Image: "/android.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "IOS",
+    Image: "/apple.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Figma",
+    Image: "/figma.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Git",
+    Image: "/git.png",
+    width: 80,
+    height: 80,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/instagram.svg",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+  },
+  {
+    name: "Instagram",
+    src: "/discord.svg",
+  },
+];
+export const Projects = [
+  {
+    title: "Modern Nextjs Website",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/NextWebsite.png",
+  },
+  {
+    title: "Space Themed Website",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/SpaceWebsite.png",
+  },
+  {
+    title: "Modern Nextjs Portfolio",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/WebPortfolio.png",
+  },
+  {
+    title: "Matrix themed Website",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    src: "/Matrix.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
